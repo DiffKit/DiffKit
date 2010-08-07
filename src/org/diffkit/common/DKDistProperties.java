e750ca41f8bff5f9028ddc8a54870e79833ba884
trurl00 <trurl00@803a2b45-1f54-7447-62a8-47d7532434bb>
2010-08-07 20:12:58 +0000

    standalone application is now able to read and process plan.xml files.

---
+++ src/org/diffkit/common/DKDistProperties.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright 2010 Joseph Panico
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.diffkit.common;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author jpanico
+ */
+public class DKDistProperties {
+   private static final String FILE_RESOURCE_PATH = "conf/dist.properties";
+
+   private static final Logger LOG = LoggerFactory.getLogger(DKDistProperties.class);
+
+   private static Properties _properties;
+   private static String _versionString;
+   private static String _publicVersionString;
+   private static String _buildDateString;
+
+   public synchronized static String getVersionString() {
+      if (_versionString != null) {
+         return _versionString;
+      }
+      Properties properties = getProperties();
+      if (properties == null) {
+         LOG.warn("couldn't find properties");
+         return null;
+      }
+      String versionBuild = properties.getProperty("version.build");
+      _versionString = getPublicVersionString() + "-" + versionBuild;
+
+      return _versionString;
+   }
+
+   public synchronized static String getPublicVersionString() {
+      if (_publicVersionString != null) {
+         return _publicVersionString;
+      }
+      Properties properties = getProperties();
+      if (properties == null) {
+         LOG.warn("couldn't find properties");
+         return null;
+      }
+      String versionMajor = properties.getProperty("version.major");
+      String versionMinor = properties.getProperty("version.minor");
+      String versionPatch = properties.getProperty("version.patch");
+      _publicVersionString = versionMajor + "." + versionMinor + "." + versionPatch;
+
+      return _publicVersionString;
+   }
+
+   public synchronized static String getBuildDateString() {
+      if (_buildDateString == null) {
+         Properties properties = getProperties();
+         if (properties == null) {
+            LOG.warn("couldn't find properties");
+            return null;
+         }
+         _buildDateString = properties.getProperty("version.date");
+      }
+      return _buildDateString;
+   }
+
+   private static Properties getProperties() {
+      if (_properties == null) {
+         ClassLoader classLoader = DKDistProperties.class.getClassLoader();
+         try {
+            InputStream propertiesStream = classLoader.getResourceAsStream(FILE_RESOURCE_PATH);
+            if (propertiesStream == null) {
+               LOG.warn("couldn't not find properties file: " + FILE_RESOURCE_PATH);
+               return null;
+            }
+            _properties = new Properties();
+            _properties.load(propertiesStream);
+            LOG.debug("_properties: " + _properties);
+         }
+         catch (Exception e_) {
+            LOG.error(null, e_);
+         }
+      }
+      return _properties;
+   }
+}
--- src/org/diffkit/diff/conf/DKTool.java
+++ src/org/diffkit/diff/conf/DKTool.java
@@ -15,25 +15,38 @@
  */
 package org.diffkit.diff.conf;
 
+import java.io.File;
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractXmlApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import org.diffkit.common.DKDistProperties;
 import org.diffkit.diff.engine.DKDiffEngine;
 
 /**
  * @author jpanico
  */
 public class DKTool {
+   private static final String PLAN_FILE_NAME_REGEX = ".*\\.plan\\.xml";
+   private static final Pattern PLAN_FILE_PATTERN = Pattern.compile(PLAN_FILE_NAME_REGEX);
    private static final Logger LOG = LoggerFactory.getLogger(DKTool.class);
 
    public static void main(String[] args_) {
-      LOG.info("args_->{}", Arrays.toString(args_));
-      ApplicationContext context = new ClassPathXmlApplicationContext(args_);
+      LOG.debug("args_->{}", Arrays.toString(args_));
+      validateArgs(args_);
+      String planFilePath = args_[0];
+
+      AbstractXmlApplicationContext context = getContext(planFilePath);
       LOG.info("context->{}", context);
+      context.setClassLoader(DKTool.class.getClassLoader());
+      context.refresh();
       DKPlan plan = (DKPlan) context.getBean("plan");
       LOG.info("plan->{}", plan);
       if (plan == null)
@@ -50,5 +63,39 @@ public class DKTool {
          LOG.error(null, e_);
          System.exit(-1);
       }
+   }
+
+   /**
+    * @param planFilePath_
+    *           can be either a FS file path (relative or absolute) or it can be
+    *           a resource style path that will be resolved via the classpath
+    */
+   private static AbstractXmlApplicationContext getContext(String planFilePath_) {
+      File file = new File(planFilePath_);
+      if (file.canRead())
+         return new FileSystemXmlApplicationContext(new String[] { planFilePath_ }, false);
+      return new ClassPathXmlApplicationContext(planFilePath_);
+   }
+
+   private static void validateArgs(String[] args_) {
+      if ((ArrayUtils.isEmpty(args_) || args_.length > 1))
+         exitWithUsage();
+      String arg = StringUtils.trimToNull(args_[0]);
+      if (arg == null)
+         exitWithUsage();
+      if (arg.equals("-version"))
+         exitWithVersion();
+      if (!PLAN_FILE_PATTERN.matcher(arg).matches())
+         exitWithUsage();
+   }
+
+   private static void exitWithVersion() {
+      System.out.println("version->" + DKDistProperties.getPublicVersionString());
+      System.exit(0);
+   }
+
+   private static void exitWithUsage() {
+      System.err.println("args must be: [-version | <xml plan file name>]");
+      System.exit(-1);
    }
 }
