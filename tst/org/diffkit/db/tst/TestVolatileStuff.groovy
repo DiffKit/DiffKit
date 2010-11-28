dd9a42674bf15aae56a3ce436148239e3108c7e6
trurl00@gmail.com <trurl00@gmail.com@803a2b45-1f54-7447-62a8-47d7532434bb>
2010-11-28 02:26:08 +0000

    changed connectionInfo for new SQLServer test DB.

--- tst/org/diffkit/db/tst/TestVolatileStuff.groovy
+++ tst/org/diffkit/db/tst/TestVolatileStuff.groovy
@@ -100,8 +100,8 @@ public class TestVolatileStuff extends GroovyTestCase {
       assert tables[0]
    }
    
-   public void tXstSQLServer(){
-      DKDBConnectionInfo connectionInfo = ['sqlserver', DKDBFlavor.SQLSERVER,'test', '10.0.1.11', 1433, 'diffkit', 'diffkit']
+   public void testSQLServer(){
+      DKDBConnectionInfo connectionInfo = ['sqlserver', DKDBFlavor.SQLSERVER,'test', '10.0.1.8', 1433, 'diffkit', 'diffkit']
       println "connectionInfo->$connectionInfo"
       DKDatabase database = [connectionInfo]
       def connection = database.connection
@@ -158,7 +158,7 @@ public class TestVolatileStuff extends GroovyTestCase {
       assert tables[0].tableName == 'TABLES'
    }
    
-   public void tXstOracle(){
+   public void testOracle(){
       DKDBConnectionInfo connectionInfo = ['oracle', DKDBFlavor.ORACLE,'XE', '10.0.1.8', 1521, 'diffkit', 'diffkit']
       println "connectionInfo->${connectionInfo.JDBCUrl}"
       DKDatabase database = [connectionInfo]
