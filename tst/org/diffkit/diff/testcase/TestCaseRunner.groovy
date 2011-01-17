8392d08909b02b3b89781847427612164559f8e9
trurl00@gmail.com <trurl00@gmail.com@803a2b45-1f54-7447-62a8-47d7532434bb>
2011-01-17 15:39:50 +0000

    update date in copyright notice.

--- tst/org/diffkit/diff/testcase/TestCaseRunner.groovy
+++ tst/org/diffkit/diff/testcase/TestCaseRunner.groovy
@@ -1,7 +1,7 @@
 package org.diffkit.diff.testcase
 
 /**
- * Copyright 2010 Joseph Panico
+ * Copyright 2010-2011 Joseph Panico
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
